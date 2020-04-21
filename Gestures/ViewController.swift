88e7f1bcd68219964c20f802532917e379a478cf
Samuel Hartman <s.hartman@setonhill.edu>
2020-04-20 22:32:47 -0400

    Initial Commit

---
+++ Gestures/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Gestures
+//
+//  Created by Sam Hartman on 4/20/20.
+//  Copyright © 2020 Sam Hartman. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
